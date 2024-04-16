f4e12f15aed73be8e34f5e2d36769eff7d15344a
Eugene <e.krotov03@gmail.com>
2024-04-16 23:11:41 +0300

    create base model

---
+++ backend/src/api/core/models/base.py
@@ -0,0 +1,29 @@
+from typing import Any
+import uuid
+
+from sqlalchemy import Integer, func
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+    _table_uuid: bool = False
+
+    @declared_attr
+    def __tablename__(self) -> str:
+        return f"{self.__name__.lower()}s"
+
+    @declared_attr.directive
+    def id(self):
+        if not self._table_uuid:
+            return mapped_column(
+                Integer, primary_key=True, autoincrement=True, nullable=False
+            )
+        return mapped_column(
+            UUID(as_uuid=True),
+            primary_key=True,
+            default=uuid.uuid4,
+            unique=True,
+            nullable=False,
+        )
