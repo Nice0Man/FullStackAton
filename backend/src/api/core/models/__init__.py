3bad895c1dfeb5c89e5793424694cb0e08420937
Eugene <e.krotov03@gmail.com>
2024-04-17 20:59:53 +0300

    refactoring

--- backend/src/api/core/models/__init__.py
+++ backend/src/api/core/models/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "User",
     "Token",
     "Client",
+    "db_manage",
 )
 
 from .base import Base
+from .db_manage import db_manage
 from .clients import Client
 from .tokens import Token
 from .users import User
